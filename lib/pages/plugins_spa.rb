7c0a51cd4547320f0af3fcc6414d411d0fbaef6f
GaneshSPatil <ganeshpl@thoughtworks.com>
2018-11-20 18:20:47 +0530

    Fix plugins specs to work with new compoentized plugins spa

--- lib/pages/plugins_spa.rb
+++ lib/pages/plugins_spa.rb
@@ -18,102 +18,94 @@ module Pages
   class PluginsSPA < AppBase
     set_url "#{GoConstants::GO_SERVER_BASE_URL}/admin/plugins"
 
-    element :plugin_settings, '.plugins-settings'
+    element :plugin_list_wrapper, '[data-test-id="plugins-list"]'
 
-    load_validation { has_plugin_settings? }
+    load_validation {has_plugin_settings?}
 
     def invalid_plugin_message_exists?(id, message)
-      plugin_config_element_of(id).has_selector?('dd', text: message)
+      plugin_to_test(id).find('[data-test-id="key-value-value-there-were-errors-loading-the-plugin"]').has_selector?('pre', text: message)
+    end
+
+    def plugin_to_test(id)
+      plugin_list_wrapper.find('[data-test-id="key-value-value-id"]', text: id).ancestor('[data-test-id="plugin-row"]')
     end
 
     def plugin_by_id_is_invalid?(id)
-      plugin_settings.find('.plugin-id', text: id).ancestor('.plugin.disabled').visible?
+      plugin_to_test(id)['data-test-has-error']
     end
 
     def plugin_by_id_is_valid?(id)
-      plugin_settings.find('.plugin-id', text: id).ancestor('.plugin.active').visible?
+      !plugin_to_test(id)['data-test-has-error']
     end
 
     def collapse_plugin_config(id)
-      return if plugin_settings.find('.plugin-id', text: id).ancestor('.plugin-header')[:class].include?('collapsed')
-      plugin_settings.find('.plugin-id', text: id).click
+      plugin_to_test(id).click
     rescue StandardError => e
       p "Not a valid plugin, moving ahead without collapsing. #{e.message} "
     end
 
     def expand_plugin_config(id)
-      return if plugin_settings.find('.plugin-id', text: id).ancestor('.plugin-header')[:class].include?('expanded')
-      plugin_settings.find('.plugin-id', text: id).click
+      plugin_to_test(id).click
     rescue StandardError => e
       p "Not a valid plugin, moving ahead without expanding. #{e.message} "
     end
 
     def is_expected_version?(id, version)
-      plugin_settings.find('.plugin-id', text: id)
-                     .ancestor('.plugin-header').find('.plugin-version').has_selector?('.value', text: version)
+      plugin_to_test(id).find('[data-test-id="key-value-value-version"]').has_selector?('pre', text: version)
     end
 
     def is_expected_name?(id, name)
-      plugin_settings.find('.plugin-id', text: id)
-                     .ancestor('.plugin-header').has_selector?('div', text: name)
+      plugin_to_test(id).find('[data-test-id="plugin-name"]').text.eql?(name)
     end
 
     def is_expected_description?(id, desc)
-      parent_for_given_label(id, 'Description').find('span').text == desc
+      plugin_to_test(id).find('[data-test-id="key-value-value-description"]').has_selector?('pre', text: desc)
     end
 
     def is_expected_author?(id, author)
-      parent_for_given_label(id, 'Author').find('span').text == author
+      plugin_to_test(id).find('[data-test-id="key-value-value-author"]').has_selector?('a', text: author)
     end
 
     def is_expected_author_link?(id, author, link)
-      parent_for_given_label(id, 'Author').find('a', text: author)[:href] == link
+      plugin_to_test(id).find('[data-test-id="key-value-value-author"]').find('a', text: author)[:href] == link
     end
 
     def is_author_link_disabled?(id, author, _link)
-      !plugin_config_element_of(id).has_selector?('a', text: author)
+      !plugin_to_test(id).find('[data-test-id="key-value-value-author"]').find('a', text: author)[:href] == link
     end
 
     def is_expected_installed_path?(id, path)
-      parent_for_given_label(id, 'Plugin file location').find('span').has_text?(path)
+      plugin_to_test(id).find('[data-test-id="key-value-value-plugin-file-location"]').has_selector?('pre', text: path)
     end
 
     def is_expected_supported_os?(id, os)
-      parent_for_given_label(id, 'Supported operating systems').find('span').text == os
+      plugin_to_test(id).find('[data-test-id="key-value-value-supported-operating-systems"]').text.eql?(os)
     end
 
     def is_expected_go_version?(id, version)
-      parent_for_given_label(id, 'Target Go Version').find('span').text == version
+      plugin_to_test(id).find('[data-test-id="key-value-value-target-go-version"]').has_selector?('pre', text: version)
     end
 
     def is_expected_bundled?(id, bundled)
-      parent_for_given_label(id, 'Bundled').find('span').text == bundled
-    end
-
-    def plugin_config_element_of(id)
-      plugin_settings.find('.plugin-id', text: id)
-                     .ancestor('.plugin.expanded').find('.plugin-config-read-only')
-    end
-
-    def plugin_actions_of(id)
-      plugin_settings.find('.plugin-id', text: id)
-                     .ancestor('.plugin-header').find('.plugin-actions')
+      plugin_to_test(id).find('[data-test-id="key-value-value-bundled"]').has_selector?('pre', text: bundled)
     end
 
     def is_plugins_settings_enabled?(id)
-      plugin_actions_of(id).has_selector?('.edit-plugin')
+      !is_plugins_settings_disabled?(id)
     end
 
     def is_plugins_settings_disabled?(id)
-      plugin_actions_of(id).has_selector?('.edit-plugin.disabled')
+      plugin_to_test(id).find('[data-test-disabled-element]')
+      true
+    rescue
+      false
     end
 
     def is_plugins_settings_displayed?(id)
-      plugin_settings.all('.plugin-header').select { |plugin| plugin.all('dd.value').last.text.eql? id }.last.has_css?('.edit-plugin')
-    end
-
-    def parent_for_given_label(id,label_text)
-      plugin_config_element_of(id).find('label' , text: label_text).find(:xpath , '..')
+      plugin_to_test(id).find('[data-test-id="edit-plugin-settings"]')
+      true
+    rescue
+      false
     end
   end
 end

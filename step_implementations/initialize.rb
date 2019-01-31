114c180cb8923475fa1e763642bb52570289477b
rajiesh <rajiesh.narayanan@gmail.com>
2019-01-31 15:51:22 +0530

    Reduce default_max_wait_time
    Since its slowing down specs which needs to verify that certain actions does not exists

--- step_implementations/initialize.rb
+++ step_implementations/initialize.rb
@@ -59,7 +59,7 @@ end
 
 Capybara.configure do |config|
   config.save_path = 'screenshots'
-  config.default_max_wait_time = 60
+  config.default_max_wait_time = 20
 end
 
 Capybara.register_driver :selenium do |app|

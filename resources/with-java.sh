3f1ec26715af8299f63003d9e310dd79602a2bf6
rajiesh <rajiesh.narayanan@gmail.com>
2019-05-07 14:33:25 +0530

    Run regression on Java 12

--- resources/with-java.sh
+++ resources/with-java.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# 3, because that's the number of JVMs we test on currently
+# 4, because that's the number of JVMs we test on currently - 8, 10, 11, 12
 # its decided not to test on Java 9 since its already EOL
-mod="$((${GO_PIPELINE_COUNTER} % 3))"
+mod="$((${GO_PIPELINE_COUNTER} % 4))"
 
 function use_jdk() {
     if [ ! -f "${HOME}/.jabba/jabba.sh" ]; then
@@ -20,9 +20,10 @@ elif [ "$mod" = "1" ]; then
   use_jdk "openjdk@1.10.0-2" "tgz+https://nexus.gocd.io/repository/s3-mirrors/local/jdk/openjdk-10.0.2_linux-x64_bin.tar.gz"
 elif [ "$mod" = "2" ]; then
   use_jdk "openjdk@1.11.0-28" "tgz+https://nexus.gocd.io/repository/s3-mirrors/local/jdk/openjdk-11-28_linux-x64_bin.tar.gz"
+elif [ "$mod" = "3" ]; then
+  use_jdk "openjdk@1.12.0" "tgz+https://download.java.net/java/GA/jdk12/GPL/openjdk-12_linux-x64_bin.tar.gz"
 fi
 
 echo "JAVA_HOME set to : $JAVA_HOME"
 
 exec "$@"
-

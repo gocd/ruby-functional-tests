dfc16e97d5d5bdd95c8e9057bbab72961a910eef
adityasood <adityas@thoughtworks.com>
2019-08-16 13:23:54 +0530

    comment screen grabber section

--- step_implementations/initialize.rb
+++ step_implementations/initialize.rb
@@ -45,13 +45,13 @@ Gauge.configure do |config|
   config.include Helpers::SpecHelper
   config.include Helpers::GoUrlHelper
   config.include Helpers::Wait
-  config.screengrabber = -> {
+#  config.screengrabber = -> {
 #    Capybara.page.save_screenshot
 #    file = File.open(Dir.glob('screenshots/*.png').first, 'rb')
 #    file_content = File.binread(file.path)
 #    #FileUtils.rm_r 'screenshots', force: true
 #    return file_content
- }
+# }
 end
 
 Capybara.configure do |config|

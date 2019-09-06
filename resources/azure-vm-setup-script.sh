3e2e8e26cbfa3f4fb774fdd4af49b4451995e9ae
adityasood <adityas@thoughtworks.com>
2019-09-06 15:04:27 +0530

    move to installing gauge using zip file

--- resources/azure-vm-setup-script.sh
+++ resources/azure-vm-setup-script.sh
@@ -11,9 +11,7 @@ curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | tee
 AZ_REPO=$(lsb_release -cs) && echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | tee /etc/apt/sources.list.d/azure-cli.list
 curl -L https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 apt-get update && apt-get install -y azure-cli
-apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-keys 023EDB0B
-echo deb https://dl.bintray.com/gauge/gauge-deb stable main | tee -a /etc/apt/sources.list
-apt-get update && apt-get install -y gauge
+curl -SsL https://downloads.gauge.org/stable | sh
 apt-get install -y docker.io
 wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz
 sh -c 'tar -x geckodriver -zf geckodriver-v0.24.0-linux64.tar.gz -O > /usr/bin/geckodriver'

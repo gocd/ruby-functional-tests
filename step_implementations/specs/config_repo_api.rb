8beb632c1d92cabef439b19b23ea9116ee40dbb1
rajiesh <rajiesh.narayanan@gmail.com>
2020-01-08 09:44:40 +0530

    Add steps for v3 config repo endpoints

--- step_implementations/specs/config_repo_api.rb
+++ step_implementations/specs/config_repo_api.rb
@@ -81,6 +81,21 @@ step 'Delete config repo <id> should return forbidden' do |id|
   end
 end
 
+step 'Get status of config repo <id> should return success' do |id|
+  response = get_status(id)
+	assert_equal response.code, 200, "Failed to get config repo status. Response code #{response.code}, body #{response.body}"
+end
+
+step 'Get definition of config repo <id> should return success' do |id|
+	response = get_definitions(id)
+	assert_equal response.code, 200, "Failed to get config repo defenition. Response code #{response.code}, body #{response.body}"
+end
+
+step 'Trigger update of config repo <id> should return success' do |id|
+	response = trigger_update(id)
+	assert_equal response.code, 201, "Failed to trigger update. Response code #{response.code}, body #{response.body}"
+end
+
 private
 
 def get_all_config_repos
@@ -89,6 +104,29 @@ def get_all_config_repos
                      .merge(basic_configuration.header)
 end
 
+def get_status(id)
+  RestClient.get http_url("#{CONFIG_REPO_BASE_URL}/#{id}/status"),
+                                    { accept: CONFIG_REPO_API_VERSION }
+                                      .merge(basic_configuration.header)   do |response, _request, _result|
+		response
+  end
+end
+
+def get_definitions(id)
+  RestClient.get http_url("#{CONFIG_REPO_BASE_URL}/#{id}/definitions"),
+                                    { accept: CONFIG_REPO_API_VERSION }
+                                      .merge(basic_configuration.header)   do |response, _request, _result|
+		response
+  end
+end
+
+def trigger_update(id)
+  RestClient.post http_url("#{CONFIG_REPO_BASE_URL}/#{id}/trigger_update"), '',
+                                    { accept: CONFIG_REPO_API_VERSION, 'X-GoCD-Confirm': true }
+                                      .merge(basic_configuration.header)   do |response, _request, _result|
+		response
+  end
+end
 
 def get_config_repo(id)
   RestClient.get http_url("#{CONFIG_REPO_BASE_URL}/#{id}"),

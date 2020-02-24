470766b7c37489240193552a3bb534e0c2dd88b6
rajiesh <rajiesh.narayanan@gmail.com>
2020-02-24 15:28:31 +0530

    Fix gauge installation failure

--- resources/azure-vm-setup-script.sh
+++ resources/azure-vm-setup-script.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 function install_gauge() {
-cat <<-EOF > /etc/yum.repos.d/gauge-stable.repo
-[gauge-stable]
-name=gauge-stable
-baseurl=http://dl.bintray.com/gauge/gauge-rpm/gauge-stable
-gpgcheck=0
-enabled=1
-EOF
-yum install --assumeyes gauge-1.0.6
+curl -sL https://github.com/getgauge/gauge/releases/download/v1.0.6/gauge-1.0.6-linux.x86_64.zip -o /home/gauge-1.0.6-linux.x86_64.zip
+cd /home && unzip /home/gauge-1.0.6-linux.x86_64.zip
+mv /home/gauge /usr/bin
+gauge -v
 }
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
@@ -21,7 +17,7 @@ apt-get install -y ruby ruby-ffi ruby-dev build-essential ruby-bundler zlib1g-de
 curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
 AZ_REPO=$(lsb_release -cs) && echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | tee /etc/apt/sources.list.d/azure-cli.list
 curl -L https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-apt-get update && apt-get install -y azure-cli
+apt-get update && apt-get install -y azure-cli unzip
 install_gauge
 apt-get install -y docker.io
 wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz

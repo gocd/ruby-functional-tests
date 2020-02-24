2022e90e3eadb324bc38f795fa92afe457fbe6d5
rajiesh <rajiesh.narayanan@gmail.com>
2020-02-24 14:00:42 +0530

    Install gauge 1.0.6 version in azure tests

--- resources/azure-vm-setup-script.sh
+++ resources/azure-vm-setup-script.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+function install_gauge() {
+cat <<-EOF > /etc/yum.repos.d/gauge-stable.repo
+[gauge-stable]
+name=gauge-stable
+baseurl=http://dl.bintray.com/gauge/gauge-rpm/gauge-stable
+gpgcheck=0
+enabled=1
+EOF
+yum install --assumeyes gauge-1.0.6
+}
+
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get update
@@ -11,7 +22,7 @@ curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | tee
 AZ_REPO=$(lsb_release -cs) && echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | tee /etc/apt/sources.list.d/azure-cli.list
 curl -L https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 apt-get update && apt-get install -y azure-cli
-curl -SsL https://downloads.gauge.org/stable | sh
+install_gauge
 apt-get install -y docker.io
 wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz
 sh -c 'tar -x geckodriver -zf geckodriver-v0.24.0-linux64.tar.gz -O > /usr/bin/geckodriver'

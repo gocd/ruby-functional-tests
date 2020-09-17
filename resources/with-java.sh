9dc7a06c7c2539505ad91ed92bbf0d3961b3f206
Marques Lee <marques.lee@thoughtworks.com>
2020-09-16 21:32:56 -0700

    Use JDK 14, allowing override through BUILD_ON_JDK env var

--- resources/with-java.sh
+++ resources/with-java.sh
@@ -1,22 +1,23 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# 3, because that's the number of JVMs we test on currently - 11, 12, 13
-mod="$((${GO_PIPELINE_COUNTER:-0} % 3))"
 # 5, because that's the number of DBs we test are: h2, postgres(9.6, 10, 11 and 12)
 db_mod="$((${GO_PIPELINE_COUNTER:-0} % 5))"
 
 function use_jdk() {
+    local jdk="openjdk@1.$1"
+
     if [ ! -f "${HOME}/.jabba/jabba.sh" ]; then
       curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash
     fi
     source "${HOME}/.jabba/jabba.sh"
-    jabba install "$1=$2"
-    jabba use "$1"
+
+    jabba install "$jdk"
+    jabba use "$jdk"
 }
 
 
 if  [[ "$(uname -s)" = Darwin* ]]; then
-  echo "Running on Mac OS so using system Java, not modifying it. Please ensure Java 11/12/13 is installed"
+  echo "Running on Mac OS so using system Java, not modifying it. Please ensure a supported JDK is installed"
 
   JAVA_HOME="$(/usr/libexec/java_home)"
   # ensure java is resolvable; it hasn't always been for me (marques)
@@ -25,12 +26,8 @@ if  [[ "$(uname -s)" = Darwin* ]]; then
     sed -i '' -e '/^wrapper\.java\.command=/d; /^set\.JAVA_HOME/d' "$wrapper"
     printf "wrapper.java.command=%s\nset.JAVA_HOME=%s\n" "$(command -v java)" "$JAVA_HOME" >> "$wrapper"
   done
-elif [ "$mod" = "0" ]; then
-  use_jdk "openjdk@1.11.0-28" "tgz+https://nexus.gocd.io/repository/s3-mirrors/local/jdk/openjdk-11-28_linux-x64_bin.tar.gz"
-elif [ "$mod" = "1" ]; then
-  use_jdk "openjdk@1.12.0" "tgz+https://download.java.net/java/GA/jdk12/GPL/openjdk-12_linux-x64_bin.tar.gz"
-elif [ "$mod" = "2" ]; then
-  use_jdk "openjdk@1.13.0" "tgz+https://download.java.net/java/GA/jdk13/5b8a42f3905b406298b72d750b6919f6/33/GPL/openjdk-13_linux-x64_bin.tar.gz"
+else
+  use_jdk "${BUILD_ON_JDK:-14}"
 fi
 
 echo "JAVA_HOME set to : $JAVA_HOME"

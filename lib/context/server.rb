56de32865f73e45109fcd1ab47796b7e58935710
Chad Wilson <chadw@thoughtworks.com>
2022-09-12 22:39:08 +0800

    Primarily run functional tests on centos-9 server

--- lib/context/server.rb
+++ lib/context/server.rb
@@ -165,7 +165,7 @@ module Context
         image_index = ENV['GO_JOB_RUN_INDEX'] ? ENV['GO_JOB_RUN_INDEX'].to_i - 1 : 0
         manifest.image_info_at(image_index)
       else
-        manifest.image_info_of('centos-8')
+        manifest.image_info_of('centos-9')
       end
       manifest
     end

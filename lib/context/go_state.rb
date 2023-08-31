50d76d6a8e34dfbd9364629f1aabddc4606f1ba4
Chad Wilson <chadw@thoughtworks.com>
2023-08-31 12:55:41 +0800

    Set accept header for messages API

--- lib/context/go_state.rb
+++ lib/context/go_state.rb
@@ -45,7 +45,7 @@ module Context
     end
 
     def capture_healthstate(path)
-      response = RestClient.get health_message_url, basic_configuration.header
+      response = RestClient.get health_message_url, { accept: 'application/vnd.go.cd+json' }.merge(basic_configuration.header)
       File.open("#{path}/health_message.json", 'w') {|file| file.write(response.body)}
     rescue StandardError => e
       File.open("#{path}/health_message.json", 'w') {|file| file.write(e.message)}

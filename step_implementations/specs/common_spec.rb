c41d477496cc3376f3b97ae9067a92b25110eb70
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2025-07-29 12:18:59 +0800

    Add back post stopjob sleep hack
    Seems we may still need this.

--- step_implementations/specs/common_spec.rb
+++ step_implementations/specs/common_spec.rb
@@ -38,8 +38,8 @@ step 'Create a <file> file' do |file|
 end
 
 step 'Create a <file> file and validate pipeline completed' do |file|
-  go_agents.create_stopjob(file)
   sleep 20 # FIXME Ensure the agent has picked up the job first
+  go_agents.create_stopjob(file)
   new_pipeline_dashboard_page.wait_till_pipeline_complete
   go_agents.delete_stopjobs
 end

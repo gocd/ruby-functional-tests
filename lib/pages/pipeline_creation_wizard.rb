21979221582f5f15d13ed742230ca9ab37cb0b50
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2025-08-11 18:53:42 +0800

    Wait longer for connection tests

--- lib/pages/pipeline_creation_wizard.rb
+++ lib/pages/pipeline_creation_wizard.rb
@@ -59,7 +59,7 @@ module Pages
     end
 
     def connection_ok?
-      page.has_css?("[data-test-id='flash-message-success']")
+      page.has_css?("[data-test-id='flash-message-success']", wait: 10)
     end
 
     def set_polling_off(_material_type)

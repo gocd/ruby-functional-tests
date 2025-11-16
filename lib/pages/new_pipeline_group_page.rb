d6036cc25323d20ba1b6fc02563aa030f81474d4
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2025-11-17 00:16:45 +0800

    Fix button enable waits

--- lib/pages/new_pipeline_group_page.rb
+++ lib/pages/new_pipeline_group_page.rb
@@ -90,12 +90,12 @@ module Pages
     end
 
     def click_on_save
-      assert_true page.has_save?(disabled: false, wait: 5)
+      assert_true has_save?(disabled: false, wait: 5)
       save.click
     end
 
     def click_on_create
-      assert_true page.has_create?(disabled: false, wait: 5)
+      assert_true has_create?(disabled: false, wait: 5)
       create.click
     end
 
